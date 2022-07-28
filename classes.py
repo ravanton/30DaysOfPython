aa56416479ed7d48348994cf4e881d32d0a3f9c9
ravanton <antonrekhva@gmail.com>
2022-07-28 22:27:16 +0300

    First commit

---
+++ classes.py
@@ -0,0 +1,10 @@
+class Flight():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.passengers = []
+
+def add_passenger(self, name):
+    if not self.open_seats():
+        return False
+    self.passengers.append(name)
+    return True
