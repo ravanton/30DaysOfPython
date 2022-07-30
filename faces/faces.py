cca862d74a28240c787bdf5b83790903ee8bd492
ravanton <antonrekhva@gmail.com>
2022-07-30 14:56:29 +0300

    First commit

---
+++ faces/faces.py
@@ -0,0 +1,15 @@
+#
+def main():
+    #Get User's input
+
+    #Call convert function 
+
+def convert(msg):
+    #Replace :) for happy emoji
+
+    #Replace :( for sad emoji
+
+    #Print string
+
+
+main()
