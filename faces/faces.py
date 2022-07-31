11f36d53005bf61ed73b4653e5249605ad6494c1
ravanton <antonrekhva@gmail.com>
2022-07-31 17:22:51 +0300

    Making Faces

--- faces/faces.py
+++ faces/faces.py
@@ -1,15 +1,19 @@
-#
+#Make faces
 def main():
     #Get User's input
-
+    msg = input("You'r happy or sad? ")
+    
     #Call convert function 
-
+    result = convert(msg)
+   
+    #Print the result 
+    print(result)
 def convert(msg):
     #Replace :) for happy emoji
-
+    msg_happy = msg.replace(":)", "🙂")
     #Replace :( for sad emoji
-
-    #Print string
-
+    msg_sad = msg_happy.replace(":(", "🙁")
+    #Return string
+    return msg_sad
 
 main()
