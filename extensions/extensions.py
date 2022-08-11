11c9ac3814a5f844dd15064c8b14c05c2bcfb1b8
ravanton <antonrekhva@gmail.com>
2022-08-11 23:25:40 +0300

    Corrected

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,13 +2,14 @@
 filename = input("File name: ").strip().lower()
 
 # If .gif or .png or .jpg or .jpeg, print "image/type"
-if ".gif" or ".png" or ".jpg" or ".jpeg" in filename:
+if ".gif" in filename:
     print("image/gif")
-elif ".png"  in filename:
+elif ".png" in filename:
     print("image/png")
-elif ".jpg" or ".jpeg" in filename:
+elif ".jpg" in filename:
     print("image/jpeg")
-
+elif ".jpeg" in filename:
+    print("image/jpeg")
 # If .pdf or .zip , print "application/type"
 elif ".pdf" in filename:
     print("application/pdf")
@@ -16,7 +17,7 @@ elif ".zip" in filename:
     print("application/zip")
 # If txt, print "text/type"
 elif ".txt" in filename:
-    print("text/type")
+    print("text/plain")
 
 #Otherwise, print "application/octec-stream"
 else:
