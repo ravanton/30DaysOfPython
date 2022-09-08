33fb8b824ce40c61e8c5b09c0b23752517f37336
ravanton <antonrekhva@gmail.com>
2022-09-08 20:39:08 +0300

    Calculat the percentage

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -8,3 +8,6 @@ while True():
         # Convert into integer 
         new_numerator = int(numerator)
         new_denominator =int(denominator)
+        # Calculat the percentage
+        f = new_numerator / new_denominator
+        # Check if it less than 1 stop the loop 
