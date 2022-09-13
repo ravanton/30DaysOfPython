df6225353ec969671160f1028f5b9244de2dad15
ravanton <antonrekhva@gmail.com>
2022-09-13 22:29:59 +0300

    Check if item is already in the dictionary

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -5,4 +5,7 @@ while True:
     try:
         # Get user input
         item = input()
-        
+        # Check if item is already in the dictionary 
+        if item in grocery:
+            # If it is, add 1 in the count 
+            
