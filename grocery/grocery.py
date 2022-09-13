910cbc7790e1d1e9ff1f5b8a7f465a83aa6a4947
ravanton <antonrekhva@gmail.com>
2022-09-13 22:36:09 +0300

    Print all the items in alphabetical order

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -8,4 +8,7 @@ while True:
         # Check if item is already in the dictionary 
         if item in grocery:
             # If it is, add 1 in the count 
-            
+            grocery[item] += 1
+            # Otherwise, add the item for the first time 
+            else:
+                grocery[item] = 1
