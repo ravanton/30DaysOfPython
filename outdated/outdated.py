6f37d2e5eed0bbcccb906c8cc04b9d38f552a472
ravanton <antonrekhva@gmail.com>
2022-09-14 23:48:18 +0300

    First commit

---
+++ outdated/outdated.py
@@ -0,0 +1,15 @@
+# Create list with the name of all months
+months =     [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
