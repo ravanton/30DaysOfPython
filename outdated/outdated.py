abc03fc1dcf06941d183febe9440d01b6de4a1ef
ravanton <antonrekhva@gmail.com>
2022-09-15 00:00:34 +0300

    Check if month is in between of 1 and  12 and day between 1 and 1

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -20,3 +20,5 @@ while True:
     try:
         # Split the date by /
        month, day, year = date.split("/")
+       # Check if month is in between of 1 and  12 and day between 1 and 1
+       if( int(month) >= 1 and int(month) <= 12 and int(day) >= 1 and int(day) <= 31 )
