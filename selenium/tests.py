c78e1ff33420b21fb45fed2e676f7907d3ac9950
ravanton <antonrekhva@gmail.com>
2022-09-17 15:58:27 +0300

    First commit

---
+++ selenium/tests.py
@@ -0,0 +1,13 @@
+import os
+import pathlib
+import unittest
+
+from selenium import webdriver
+
+# Finds the Uniform Resourse Identifier of a file
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
+
+# Sets up web driver using Google chrome
+driver = webdriver.Chrome()
+
