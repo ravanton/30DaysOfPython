e48293e29f55601e07f61b7a0360aadebe9ef2f4
ravanton <antonrekhva@gmail.com>
2022-11-09 19:46:00 +0300

    upload 3day.py

---
+++ day3.py
@@ -0,0 +1,11 @@
+print(3 > 2) # True, because 3 is greater than 2
+print(3 >= 2)
+print(3 == 2) # False, because 3 is not equal to 2
+print(3 != 2) # True, because 3 is not equal to 2
+
+print(len('python') > len('dragon')) # False
+
+
+# Comparing something gives either a True or False # Сравнение чего-либо дает либо истинное, либо ложное значение
+print('True == True: ', True == True)
+print("False == True:", True == False)
