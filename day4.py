76ca916cdd06f86943756881989065141a7cf2d3
ravanton <antonrekhva@gmail.com>
2022-11-13 15:08:16 +0300

    Use the string formatting method

--- day4.py
+++ day4.py
@@ -1,3 +1,17 @@
+# Интерполяция строк
 a = 8
 b = 6
 print(f"{a} + {b} = {a + b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b:.2f}")
+print(f'{a} % {b} = {a % b}')
+print(f'{a} // {b} = {a // b}')
+print(f'{a} ** {b} = {a ** b}')
+
+# Use the string formatting method to display the following:
+radius = 10
+area = 3.14 * radius ** 2
+print(f"The area of a circle with radius {radius} is {area:.0f} meters square.")
+
+# Use a tab escape sequence to write the following lines
+print("Name\tAge\tCountry\tCity")
