7f49a2ba4a8c87e150edb343b4acaebb681c1799
ravanton <antonrekhva@gmail.com>
2022-11-14 20:48:16 +0300

    Inserting Items into a List

--- day5_list.py
+++ day5_list.py
@@ -1,6 +1,6 @@
 #Unpacking List Items
-lst = ['item','item2','item3', 'item4', 'item5']
-first_item, second_item, third_item, *rest = lst
+item = ['item','item2','item3', 'item4', 'item5']
+first_item, second_item, third_item, *rest = item
 print(first_item)     # item1
 print(second_item)    # item2
 print(third_item)     # item3
