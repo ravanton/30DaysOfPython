42972ec92afb96d5afc9c11fb58ef50bf2fc7cd0
ravanton <antonrekhva@gmail.com>
2022-11-14 20:40:44 +0300

    unpackind list

---
+++ day5_list.py
@@ -0,0 +1,7 @@
+#Unpacking List Items
+lst = ['item','item2','item3', 'item4', 'item5']
+first_item, second_item, third_item, *rest = lst
+print(first_item)     # item1
+print(second_item)    # item2
+print(third_item)     # item3
+print(rest) 
