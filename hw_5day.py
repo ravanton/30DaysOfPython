439d698111c38e31b98162be6267aa787f80ad2b
ravanton <antonrekhva@gmail.com>
2022-11-15 20:15:24 +0300

    ending 5 exercises

---
+++ hw_5day.py
@@ -0,0 +1,15 @@
+# Declare an empty list
+empty_list = []
+# Declare a list with more than 5 items
+list_5_item = ["web3", "python3", "js", "bitcoin", "git"]
+
+# Find the length of your list
+print("Length my list: ", len(list_5_item))
+
+# Get the first item, the middle item and the last item of the list
+first_item = list_5_item[0]
+middle_item = list_5_item[2]
+last_item = list_5_item[-1]
+print(last_item)
+print(first_item)
+print(middle_item)
