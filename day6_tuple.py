6b1d587acb5a211b754aaa5cd481d276968f167d
ravanton <antonrekhva@gmail.com>
2022-11-17 21:00:17 +0300

    Check if an item exists in tuple

--- day6_tuple.py
+++ day6_tuple.py
@@ -23,9 +23,27 @@ join_tupe = name_tuple + name_tuple_new
 print("", len(join_tupe))
 
 # Create fruits, vegetables and animal products tuples. Join the three tuples and assign it to a variable called food_stuff_tp.
-fruits = ['banana', 'orange', 'mango', 'lemon']
+fruits = ['Banana', 'Orange', 'Mango', 'Lemon']
 vegetables = ["Broccoli", "Garlic", "Kale" ]
 animal_products = ["Swiss chard", "Green peas"]
 
+# Join the three tuples and assign it to a variable called food_stuff_tp.
 food_stuff_tp = fruits + vegetables + animal_products
-print(food_stuff_tp)
+
+# Change the about food_stuff_tp tuple to a food_stuff_lt list (Измените кортеж about food_stuff_tp на список food_stuff_lt.)
+food_stuff_lp = list(food_stuff_tp)
+# Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+middle_item = food_stuff_tp[4]
+print(middle_item)
+# Slice out the first three items and the last three items from food_staff_lt list
+first_three_items = food_stuff_tp[3:6]
+print(first_three_items)
+
+# Полностью удалить кортеж food_staff_tp
+del food_stuff_tp
+
+# Check if an item exists in tuple:
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
