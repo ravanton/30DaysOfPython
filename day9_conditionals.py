23181889f6d093c2d7d9689bff5db4c5bb44b49c
ravanton <antonrekhva@gmail.com>
2022-11-21 20:20:59 +0300

    done exercises 2

--- day9_conditionals.py
+++ day9_conditionals.py
@@ -49,10 +49,20 @@ elif a == 0:
 else:
     print('A is negative')
 
-# Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
+# 1 Get user input using input(“Enter your age: ”). If user is 18 or older, give feedback: You are old enough to drive. If below 18 give feedback to wait for the missing amount of years. Output:
 years = int(input("Enter your age: "))
-years_learn_driver = 18 - years
+years_learn_to_driver = 18 - years
+
 if years >= 18:
     print("You are old enough to drive.\nOutput:") 
 else:
-    print(f"You need {years_learn_driver} more years to learn to drive.")
+    print(f"You need {years_learn_to_driver} more years to learn to drive.")
+
+# 2 Compare the values of my_age and your_age using if … else. Who is older (me or you)? Use input(“Enter your age: ”) to get the age as input. You can use a nested condition to print 'year' for 1 year difference in age, 'years' for bigger differences, and a custom text if my_age = your_age. Output:
+your_age = int(input("Enter your afe: "))
+my_age = 22
+older_me = your_age - my_age
+if my_age == your_age:
+    print("Yep it's my years!")
+else:
+    print(f"You are {older_me} years older than me.")
