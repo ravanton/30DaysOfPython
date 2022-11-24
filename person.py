d3cd038af6ac57440091aa8926e03ccb7f0ca429
ravanton <antonrekhva@gmail.com>
2022-11-24 20:57:58 +0300

    finished exercises: level 3

--- person.py
+++ person.py
@@ -1,11 +1,13 @@
+#   * If a person skills has only JavaScript and React, print('He is a front end developer'), if the person skills has Node, Python, MongoDB, print('He is a backend developer'), if the person skills has React, Node and MongoDB, Print('He is a fullstack developer'), else print('unknown title') - for more accurate results more conditions can be nested!
+
 person = {
     'first_name': 'Asabeneh',
     'last_name': 'Yetayeh',
     'age': 250,
     'country': 'Finland',
     'is_marred': True,
-    # 'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
-    'skills': [ 'React', 'MongoDB', 'Node'],
+    'skills': ['JavaScript', 'React', 'Node', 'MongoDB', 'Python'],
+    
     'address': {
         'street': 'Space street',
         'zipcode': '02210'
@@ -20,3 +22,8 @@ elif 'Node' in person['skills'] and 'React' in person['skills'] and 'MongoDB' in
     print('He is fullstack developer')
 else:
     print('not a developer')
+
+#  * If the person is married and if he lives in Finland, print the information in the following format:
+
+if  person['is_marred'] == True and 'Finland' in person['country']:
+    print(f"{person['first_name']} {person['last_name']} lives in {person['country']}. He is married.")
