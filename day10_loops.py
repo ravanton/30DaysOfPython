3fcb06198450bc4e9db516f836b793f7ed30f6cf
ravanton <antonrekhva@gmail.com>
2022-11-25 20:27:30 +0300

    Iterate 0 to 10 using for loop, do the same using while loop.

---
+++ day10_loops.py
@@ -0,0 +1,14 @@
+# Iterate 0 to 10 using for loop, do the same using while loop.
+
+
+# for number in range(11):
+    # print(number)
+
+
+number = -1
+
+while number < 10:
+    number += 1
+    print(number)
+
+
