47a63d30c3140c02156b6a7ca1c76b67f11f593e
ravanton <antonrekhva@gmail.com>
2022-11-27 13:56:21 +0300

    square pattern

--- day10_loops.py
+++ day10_loops.py
@@ -11,8 +11,15 @@ while number < 10:
     number += 1
     print(number)
 
-
+# Write a loop that makes seven calls to print(), so we get on the output the following triangle:
 for a in range(7):
     for x in range(a + 1):
-        print("@", end="")
+        print("@ ", end="")
     print("\n")
+
+# Use nested loops to create the following:
+print("Star square pattern: ")
+for a in range(8):
+    for x in range(8):
+        print("@ ", end="")
+    print('\n')
