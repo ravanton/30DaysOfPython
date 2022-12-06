f36475eb1956aabd32dc7ab0aae05e9ce3dbd283
ravanton <antonrekhva@gmail.com>
2022-12-06 21:08:49 +0300

    pi as Pi

--- day12_modules.py
+++ day12_modules.py
@@ -2,5 +2,25 @@
 def generate_full_name(firstname, lastname):
     return firstname + ' ' + lastname
 
-import house
-print(house.generate_full_name("anton", "rek"))
+
+
+
+# # import the module
+# import os
+# # Creating a directory
+# # os.mkdir('directory_name')
+# # Changing the current directory
+# os.chdir('path')
+# # Getting current working directory
+# os.getcwd()
+# # Removing directory
+# os.rmdir()
+from statistics import * # importing all the statistics modules
+ages = [20, 20, 4, 24, 25, 22, 26, 20, 23, 22, 26]
+print(mean(ages))       # ~22.9
+print(median(ages))     # 23
+print(mode(ages))       # 20
+print(stdev(ages))      # ~2.3
+
+from math import pi as  PI
+print(PI) # 3.141592653589793
