504459be1540e98a2994c7d0a11fa936a3f7dbe9
ravanton <antonrekhva@gmail.com>
2022-12-11 16:24:56 +0300

    some example

--- 13day_list_comprehension.py
+++ 13day_list_comprehension.py
@@ -19,5 +19,19 @@ print(number)
 
 # List comprehension can be combined with if expression
 # Generating even numbers
-even_number = [ i for i in range(21) if i % 2 == 0 ]
+even_number = [ i for i in range(21) if i % 2 == 0 ] # to generate even numbers in range 0 to 21
 print(even_number)
+
+# Generating odd numbers
+odd_numbers = [i for i in range(21) if i % 2 != 0]  # to generate odd numbers in range 0 to 21
+print(odd_numbers)
+
+# Filter numbers: let's filter out positive even numbers from the list below
+numbers = [-8, -7, -3, -1, 0, 1, 3, 4, 5, 7, 6, 8, 10]
+positive_even_numbers = [i for i in range(21) if i % 2 == 0 and i > 0]
+print(positive_even_numbers)
+
+# Flattening a three dimensional array
+list_of_lists = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+flattened_list = [ number for row in list_of_lists for number in row]
+print(flattened_list)
