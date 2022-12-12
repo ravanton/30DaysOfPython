aaa900fa8e3cc65b6238607f042f84e182e82fe9
ravanton <antonrekhva@gmail.com>
2022-12-12 20:34:31 +0300

    # Flatten the following list of lists of lists to a one dimensional list :

--- 13day_list_comprehension.py
+++ 13day_list_comprehension.py
@@ -57,7 +57,21 @@ print(multiple_variable(5, 5, 3)) # 22
 def power(x):
     return lambda n : x ** n
 
-cube = power(2)(3)
-print(cube)
+cube = power(2)(3) # function power now need 2 arguments to run, in separate rounded brackets
+print(cube) # 8
 two_power_of_live = power(2)(5)
 print(two_power_of_live)
+
+# Filter only negative and zero in the list using list comprehension
+
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+only_negative_and_zero  = [i for i in numbers if i <= 0 ]
+print(only_negative_and_zero)
+
+# Flatten the following list of lists of lists to a one dimensional list :
+
+list_of_lists =[[[1, 2, 3]], [[4, 5, 6]], [[7, 8, 9]]]
+
+one_dimensional_list = [ number for row in list_of_lists for number in row]
+dimensional_list = [number for row in one_dimensional_list for number in row ]
+print(dimensional_list)
