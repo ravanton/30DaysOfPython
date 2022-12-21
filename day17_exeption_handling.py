9a22ba4a0c7a9f2d9f90bfbbb59f8d14dcbc8c3a
ravanton <antonrekhva@gmail.com>
2022-12-21 21:07:38 +0300

    add file

---
+++ day17_exeption_handling.py
@@ -0,0 +1,11 @@
+# try:
+#     code in this block if things go well 
+#     код в этом блоке, если все идет хорошо
+# except:
+#     code in this block run if things go wrong
+# код в этом блоке выполнить, если все пойдет не так
+
+try:
+    print(10 + '5')
+except:
+    print('Something went wrong')
