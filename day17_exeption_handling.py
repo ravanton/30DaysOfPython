0dabc5e8e43672f224127b639d906ef64f2f94b8
ravanton <antonrekhva@gmail.com>
2022-12-21 21:18:27 +0300

    # Packing and Unpacking Arguments in Python

--- day17_exeption_handling.py
+++ day17_exeption_handling.py
@@ -9,3 +9,27 @@ try:
     print(10 + '5')
 except:
     print('Something went wrong')
+
+# try:
+#     name = input('Enter your name:')
+#     year_born = input('Year you were born:')
+#     age = 2019 - int(year_born)
+#     print(f'You are {name}. And your age is {age}.')
+# except TypeError:
+#     print('Type error occured')
+# except ValueError:
+#     print('Value error occured')
+# except ZeroDivisionError:
+#     print('zero division error occured')
+# else:
+#     print("I usually run with the try block")
+# finally:
+#     print("I alway run.")
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print('You are {name}. And your age is {age}.')
+except Exception as e:
+    print(e)
