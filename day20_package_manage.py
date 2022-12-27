cb702143380e84b285857c0b66343f3862025498
ravanton <antonrekhva@gmail.com>
2022-12-27 22:53:34 +0300

    # opening a network and fetching a data

--- day20_package_manage.py
+++ day20_package_manage.py
@@ -1,4 +1,4 @@
-import webbrowser
+import webbrowser # web browser module to open websites
 
 # list of urls: python
 url_list = [
@@ -6,5 +6,12 @@ url_list = [
     'http://www.youtube.com'
 ]
 # opens the above list of websites in a defferrent tab
-for url in url_list:
-    webbrowser.open_new_tab(url)
+# for url in url_list:
+#     webbrowser.open_new_tab(url)
+
+import requests # importing the request module
+url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt' # text from a website
+
+response = requests.get(url)
+print(response)
+print(response.status_code)
