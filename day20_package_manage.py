88fe7ee80d16c896ca7768129ea16991f2d83e2f
ravanton <antonrekhva@gmail.com>
2022-12-28 23:08:30 +0300

    touch file

--- day20_package_manage.py
+++ day20_package_manage.py
@@ -13,5 +13,6 @@ import requests # importing the request module
 url = 'https://www.w3.org/TR/PNG/iso_8859-1.txt' # text from a website
 
 response = requests.get(url)
-print(response)
-print(response.status_code)
+print(response.status_code) # status code, success:200
+print(response.headers)     # headers information
+print(response.text) # gives all the text from the page
