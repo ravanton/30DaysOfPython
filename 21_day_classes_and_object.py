6317f54f4037b460818c35b3d841eb34be32026f
ravanton <antonrekhva@gmail.com>
2022-12-29 20:32:19 +0300

    # Objects can have methods. The methods are functions which belong to the object.

--- 21_day_classes_and_object.py
+++ 21_day_classes_and_object.py
@@ -5,20 +5,35 @@
 # class ClassName:
 #   code goes here
 
-class Person:
+# class Person:
+#     def __init__(self, firstname, lastname, age, city):
+#     # self allows to attach parameter to the class
+#         # self.name = name
+#         self.firstname = firstname
+#         self.lastname = lastname
+#         self.age = age
+#         self.city = city
+
+
+
+# p = Person("Anton", "R", 22, "London")
+# # print(p.name)
+# print(p.lastname)
+# print(p.firstname)
+# print(p.age)
+# print(p.city)
+
+# Object Methods
+# Objects can have methods. The methods are functions which belong to the object.
+class Person():
     def __init__(self, firstname, lastname, age, city):
-    # self allows to attach parameter to the class
-        # self.name = name
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
         self.city = city
 
-
-
-p = Person("Anton", "R", 22, "London")
-# print(p.name)
-print(p.lastname)
-print(p.firstname)
-print(p.age)
-print(p.city)
+    def person_info(self):
+        return (f"{self.firstname} {self.lastname} is {self.age} year old. He lives in {self.city}")
+        
+p = Person('anton', "R", 22, "london")
+print(p.person_info())
