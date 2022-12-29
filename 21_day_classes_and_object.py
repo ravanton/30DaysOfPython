dbabf0991a883ca36b45a9af24cd50b2d149dedb
ravanton <antonrekhva@gmail.com>
2022-12-29 20:44:18 +0300

    give default values for the parameters in the constructor

--- 21_day_classes_and_object.py
+++ 21_day_classes_and_object.py
@@ -5,23 +5,23 @@
 # class ClassName:
 #   code goes here
 
-# class Person:
-#     def __init__(self, firstname, lastname, age, city):
-#     # self allows to attach parameter to the class
-#         # self.name = name
-#         self.firstname = firstname
-#         self.lastname = lastname
-#         self.age = age
-#         self.city = city
+class Person:
+    def __init__(self, firstname, lastname, age, city):
+    # self allows to attach parameter to the class
+        
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.city = city
 
 
 
-# p = Person("Anton", "R", 22, "London")
-# # print(p.name)
-# print(p.lastname)
-# print(p.firstname)
-# print(p.age)
-# print(p.city)
+p = Person("Anton", "R", 22, "London")
+
+print(p.lastname)
+print(p.firstname)
+print(p.age)
+print(p.city)
 
 # Object Methods
 # Objects can have methods. The methods are functions which belong to the object.
@@ -34,6 +34,23 @@ class Person():
 
     def person_info(self):
         return (f"{self.firstname} {self.lastname} is {self.age} year old. He lives in {self.city}")
-        
-p = Person('anton', "R", 22, "london")
+
+p = Person('Anton', "R", 22, "LA")
+print(p.person_info())
+
+# Object Default Methods
+# Sometimes, you may want to have a default values for your object methods. If we give default values for the parameters in the constructor, we can avoid errors when we call or instantiate our class without parameters. Let's see how it looks:
+class Person():
+    def __init__(self, firstname = 'Anton', lastname = "R", age = 22, city = "Kiev"):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.age = age
+        self.city = city
+
+    def person_info(self):
+        return (f"{self.firstname} {self.lastname} is {self.age} year old. He lives in {self.city}")
+
+p = Person()
+print(p.person_info())
+p = Person("Garry", "D", 23, "New york")
 print(p.person_info())
