8bcb1b331dbe11342bc5439ae80aa1acf53c89bb
ravanton <antonrekhva@gmail.com>
2023-01-04 22:24:32 +0300

    upload file

---
+++ day24_static.py
@@ -0,0 +1,3 @@
+# Python for Statistical Analysis
+# Statistics
+# Statistics is the discipline that studies the collection, organization, displaying, analysing, interpretation and presentation of data. Statistics is a branch of Mathematics that is recommended to be a prerequisite for data science and machine learning
