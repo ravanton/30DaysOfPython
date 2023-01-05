9de89bc1395208cfb9321e9db237379c55299981
ravanton <antonrekhva@gmail.com>
2023-01-05 20:33:33 +0300

    # Creating float numpy arrays

--- day24_static.py
+++ day24_static.py
@@ -24,3 +24,12 @@ print(two_dimensional_list)  # [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
 numpy_array_from_list = np.array(python_list)
 print(type (numpy_array_from_list))   # <class 'numpy.ndarray'>
 print(numpy_array_from_list) # array([1, 2, 3, 4, 5])
+
+# Creating float numpy arrays
+# Creating a float numpy array from list with a float data type parameter
+
+# Python list
+python_list = [1,2,3,4,5]
+
+numy_array_from_list2 = np.array(python_list, dtype=float)
+print(numy_array_from_list2) # array([1., 2., 3., 4., 5.])
