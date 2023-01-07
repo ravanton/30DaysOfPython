49a06825cb112e11461ab3febd31b9da9dfb991b
ravanton <antonrekhva@gmail.com>
2023-01-07 14:27:20 +0300

    In the post, route we will use GET and POST method alternative depending on the type of request

--- python_for_web/app.py
+++ python_for_web/app.py
@@ -3,6 +3,8 @@ from flask import Flask, render_template, request, redirect, url_for
 import os # importing operating system module
 
 app = Flask(__name__)
+# to stop caching static file
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0 
 
 @app.route('/') # this decorator create the home route
 def home ():
@@ -15,10 +17,20 @@ def about():
     name = '30 Days Of Python Programming'
     return render_template('about.html', name = name, title = 'About Us')
 
-@app.route('/post')
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+
+@app.route('/post', methods= ['GET','POST'])
 def post():
     name = 'Text Analyzer'
-    return render_template('post.html', name = name, title = name)
+    if request.method == 'GET':
+         return render_template('post.html', name = name, title = name)
+    if request.method =='POST':
+        content = request.form['content']
+        print(content)
+        return redirect(url_for('result'))
 
 if __name__ == '__main__':
     # for deployment we use the environ
