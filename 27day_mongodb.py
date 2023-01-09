f14808a00c0c9e7dcc9f28918b1e1c8cfb38840c
ravanton <antonrekhva@gmail.com>
2023-01-09 22:30:54 +0300

    # Connecting Flask application to MongoDB Cluster

--- 27day_mongodb.py
+++ 27day_mongodb.py
@@ -1,1 +1,15 @@
 # Python is a backend technology and it can be connected with different data base applications. It can be connected to both SQL and noSQL databases. In this section, we connect Python with MongoDB database which is noSQL database.
+# Connecting Flask application to MongoDB Cluster
+# let's import the flask
+from flask import Flask, render_template
+import os # importing operating system module
+MONGODB_URI = 'mongodb+srv://asabeneh:your_password_goes_here@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
+client = pymongo.MongoClient(MONGODB_URI)
+print(client.list_database_names())
+
+app = Flask(__name__)
+if __name__ == '__main__':
+    # for deployment we use the environ
+    # to make it work for both production and development
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
